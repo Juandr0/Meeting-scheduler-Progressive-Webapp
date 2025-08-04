16db2a129c699cbb126b9779b31a3b18d66ead42
Alexander <alexander.cg.carlsson@gmail.com>
2025-08-04 13:06:45 +0200

    removed trap for time travelers trying to book in the past

--- src/components/RoomDetails/RoomHeader.tsx
+++ src/components/RoomDetails/RoomHeader.tsx
@@ -13,12 +13,25 @@ type Props = {
 export default function RoomHeader({ dates, onPrevWeek, onNextWeek }: Props) {
   const title = getHeaderDateRange(dates);
   const week = getWeekNumber(dates[0]);
+  const currentWeek = getWeekNumber(new Date());
 
   return (
     <header className='flex w-full justify-between items-center py-5 px-4 sm:px-10'>
-      <button onClick={onPrevWeek}>
-        <MdArrowBackIos size={appSizes.defaultIconSize} />
+      <button
+        onClick={onPrevWeek}
+        disabled={week <= currentWeek}
+        className={
+          week <= currentWeek
+            ? 'opacity-10 cursor-not-allowed pointer-events-none:'
+            : ''
+        }
+      >
+        <MdArrowBackIos
+          size={appSizes.defaultIconSize}
+          className={week <= currentWeek ? 'cursor-not-allowed' : ''}
+        />
       </button>
+
       <div className='flex items-center flex-col'>
         <h2 className='text-2xl'>{title}</h2>
         <h2 className='text-xl'>Week {week}</h2>
